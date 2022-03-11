af3eda7c8028be9b622bd820d7b81327c7d0569b
lasvice <undercloud@yeah.net>
2022-03-12 01:57:28 +0900

    config xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -33,7 +33,7 @@ myConfig =
     , layoutHook = myLayout
     , terminal = "alacritty"
     , borderWidth = 3
-    , focusedBorderColor = "#ff79c6" 
+    , focusedBorderColor = "#bd93f9" 
     -- , focusedBorderColor = "#007acc"
     , startupHook = myStartupHook
     , manageHook = myManageHook
@@ -84,29 +84,36 @@ myLayout = mySpacing $ tiled ||| Mirror tiled ||| Full ||| threeCol
 
     
 myXmobarPP :: PP
-myXmobarPP = def
-    { ppSep             = magenta " | "
-    , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden          = white . wrap " " ""
+myXmobarPP =
+  def
+    { ppSep = white " | "
+    , ppTitleSanitize = xmobarStrip
+    , ppCurrent = wrap " " "" . xmobarBorder "Top" "#bd93f9" 2
+    , ppHidden = white . wrap " " ""
     , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
-    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    , ppUrgent = red . wrap (yellow "!") (yellow "!")
+    , ppOrder = \[ws, _, _, wins] -> [ws, wins]
+    , ppExtras = [logTitles formatFocused formatUnfocused]
     }
   where
-    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
-
+    -- formatFocused = wrap (white "[") (white "]") . blue . ppWindow
+    -- formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . lowWhite . ppWindow
+    formatFocused = wrap (white "") (white "") . blue . ppWindow
+    formatUnfocused = wrap (lowWhite "") (lowWhite "") . lowWhite . ppWindow
     -- | Windows should have *some* title, which should not not exceed a
     -- sane length.
     ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
-
+    ppWindow =
+      xmobarRaw .
+      (\w ->
+         if null w
+           then "untitled"
+           else w) .
+      shorten 10
     blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#ff79c6" ""
-    blue     = xmobarColor "#bd93f9" ""
-    white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#f1fa8c" ""
-    red      = xmobarColor "#ff5555" ""
+    magenta = xmobarColor "#ff79c6" ""
+    blue = xmobarColor "#bd93f9" ""
+    white = xmobarColor "#f8f8f2" ""
+    yellow = xmobarColor "#f1fa8c" ""
+    red = xmobarColor "#ff5555" ""
     lowWhite = xmobarColor "#bbbbbb" ""
